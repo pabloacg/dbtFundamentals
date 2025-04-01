a967bd7daf0149af764dc4683048ec32bd5d764d
pablo.acosta@civica-soft.com <pablo.acosta@civica-soft.com>
2025-04-01 08:54:03 +0000

    excercise fct_orders and dim_customers

--- models/marts/finance/fct_orders.sql
+++ models/marts/finance/fct_orders.sql
@@ -1,19 +1,30 @@
-with 
-orders as(
-    select * from {{ ref('stg_jaffle_shop__orders') }}
+with orders as  (
+    select * from {{ ref ('stg_jaffle_shop__orders' )}}
 ),
 
-payments as(
-    select * from {{ ref('stg_stripe__payments') }}
+payments as (
+    select * from {{ ref ('stg_stripe__payments') }}
 ),
 
-final as(
+order_payments as (
+    select
+        order_id,
+        sum (case when status = 'success' then amount end) as amount
+
+    from payments
+    group by 1
+),
+
+ final as (
+
     select
         orders.order_id,
         orders.customer_id,
-        payments.amount
-    from orders    
-    left join payments using (order_id)
+        orders.order_date,
+        coalesce (order_payments.amount, 0) as amount
+
+    from orders
+    left join order_payments using (order_id)
 )
 
 select * from final
--- models/staging/stripe/stg_stripe__payments.sql
+++ models/staging/stripe/stg_stripe__payments.sql
@@ -1,7 +1,11 @@
-select 
-    id as payment_id, 
-    orderid as order_id, 
-    paymentmethod as payment_method, 
-    status, 
-    amount 
-from pacosta_raw.stripe.payment
+select
+    id as payment_id,
+    orderid as order_id,
+    paymentmethod as payment_method,
+    status,
+
+    -- amount is stored in cents, convert it to dollars
+    amount / 100 as amount,
+    created as created_at
+
+from pacosta_raw.stripe.payment 
