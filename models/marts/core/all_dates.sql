0bf6278c04a7cdb8a5fbf98f2787822b6a6088cc
Pablo Acosta <pablo.acosta@civica-soft.com>
2025-04-16 10:20:36 +0000

    packages

---
+++ models/marts/core/all_dates.sql
@@ -0,0 +1,13 @@
+{{ config(
+    materialized='table',
+) }}
+
+with date_spine as (
+    {{ dbt_utils.date_spine(
+        datepart="day",
+        start_date="cast('2020-01-01' as date)",
+        end_date="cast('2020-12-31' as date)"
+    ) }}
+)
+
+select * from date_spine
